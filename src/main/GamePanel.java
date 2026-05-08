0aecd84d3c009d5a1c813f99fc39a2b439e2296b
Rikudon <moritz.berger19@gmail.com>
2026-05-08 20:58:42 +0200

    GamePanel aus main übernommen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,7 +1,7 @@
 package main;
 
-import inputs.KeyboardInputs;
-import inputs.MouseInputs;
+import entities.Entity;
+import entities.Enemy;
 import javax.swing.JPanel;
 import java.awt.*;
 
@@ -27,10 +27,18 @@ public class GamePanel extends JPanel {
         // Spieler als blaues Rechteck zeichnen
         g.setColor(Color.BLUE);
 
-        // Wir holen uns die aktuellen Koordinaten direkt vom Player-Objekt
-        int x = game.player.getX();
-        int y = game.player.getY();
+        //alle entities werden durchgegangen und an ihrer position mit ihrer weite und breite gerendert
+        for (Entity entity : game.getEntities()){
+            g.setColor(Color.BLUE);
+            if (entity instanceof Enemy){
+                g.setColor(Color.RED);//enemies werden rot
+            }
+            double x = entity.getX();
+            double y = entity.getY();
+            int width = entity.getWidth();
+            int height = entity.getHeight();
 
-        g.fillRect(x, y, 50, 50);
+            g.fillRect((int) Math.round(x), (int) Math.round(y), width, height);//rectangle wird an der gerundeten x und y position erstellt
+        }
     }
 }
