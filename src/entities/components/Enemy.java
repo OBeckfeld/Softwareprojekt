e26aaab45b836b808f5e32c23bb1a381e4a91a03
Rikudon <moritz.berger19@gmail.com>
2026-05-08 20:53:21 +0200

    Enemy aus main übernommen

---
+++ src/entities/components/Enemy.java
@@ -0,0 +1,19 @@
+package entities;
+
+import entities.components.MovementComponent;
+import entities.managers.EntityRegistry;
+import tools.Vector;
+
+public class Enemy extends Entity {
+    Player player;//nur zum testen
+    public Enemy(int x, int y, int height, int width, EntityRegistry registry, Player player) {
+        super(x, y, height, width, registry);
+        setSpeed(8);
+        this.player = player;//nur zum testen
+    }
+    public void update(){
+        Vector vector = new Vector(getX(), getY(), player.getX(), player.getY());
+        vector.setLength(getSpeed());
+        vector.apply(this);//get getSpeed() schritte in richtung vom player
+    }
+}
