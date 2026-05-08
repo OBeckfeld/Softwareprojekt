5183f252787f0f35e88d483e448bc9d7dae6a0b7
Rikudon <moritz.berger19@gmail.com>
2026-05-08 20:51:53 +0200

    MovementComponent aus main übernommen

--- src/entities/components/MovementComponent.java
+++ src/entities/components/MovementComponent.java
@@ -1,36 +1,47 @@
 package entities.components;
 
 import entities.Entity;
+import inputs.KeyboardInputs;
 
 import java.awt.event.KeyEvent;
 import java.util.Set;
 
 public class MovementComponent {
 
+    KeyboardInputs inputs;
+
     public MovementComponent(){}
+    public MovementComponent(KeyboardInputs inputs){this.inputs = inputs;}
 
 //player muss noch zu entity geändert werden
-    public void move(Set<Integer> keyboardInputs, Entity player) {
+    public void move(Entity entity) {
         double dx = 0, dy = 0; //Veränderung der X und Y Koordinaten genau
         int vx, vy ; //Veränderung der X und Y Koordinaten auf int gerundet
 
         //prüfen, welche Inputs gegeben werden
-        if(keyboardInputs.contains(KeyEvent.VK_W)){ dy -= player.getSpeed(); }
-        if(keyboardInputs.contains(KeyEvent.VK_S)){ dy += player.getSpeed(); }
-        if(keyboardInputs.contains(KeyEvent.VK_A)){ dx -= player.getSpeed(); }
-        if(keyboardInputs.contains(KeyEvent.VK_D)){ dx += player.getSpeed(); }
+        if(inputs.getHeldKeys().contains(KeyEvent.VK_W)){ dy -= entity.getSpeed(); }
+        if(inputs.getHeldKeys().contains(KeyEvent.VK_S)){ dy += entity.getSpeed(); }
+        if(inputs.getHeldKeys().contains(KeyEvent.VK_A)){ dx -= entity.getSpeed(); }
+        if(inputs.getHeldKeys().contains(KeyEvent.VK_D)){ dx += entity.getSpeed(); }
 
         //für diagonale Bewegung
         if(dx != 0 && dy != 0){
-            vx = (int) Math.round(dx / 1.4142135623730951);
-            vy = (int) Math.round(dy / 1.4142135623730951);
-        }
-        else{
-            vx = (int )Math.round(dx);
-            vy = (int )Math.round(dy);
+            dx = dx * 0.70710678118;
+            dy = dy * 0.70710678118;
         }
 
-        player.setX(player.getX() + vx);
-        player.setY(player.getY() + vy);
+        entity.getHurtbox().setLocation(entity.getX() + dx,entity.getY() + dy);
+
+        entity.setX(entity.getX() + dx);
+        entity.setY(entity.getY() + dy);
+    }
+
+    public void move(Entity entity, double dx, double dy) {
+
+        entity.getHurtbox().setLocation(entity.getX() + dx,entity.getY() + dy);
+
+        entity.setX(entity.getX() + dx);
+        entity.setY(entity.getY() + dy);
+
     }
 }
