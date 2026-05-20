d62e61a0acfca7c883c0e9e5532f5211e5b9dca3
Vieles-code <leosdymny@gmail.com>
2026-05-20 11:04:07 +0200

    DMGBoost2 hinzugefügt

---
+++ src/skilltree/DMGBoost2.java
@@ -0,0 +1,28 @@
+package skilltree;
+
+import entities.PlayerTypeEntity;
+import entities.components.MovementComponent;
+
+public class DMGBoost2 extends Ability {
+    public DMGBoost2(PlayerTypeEntity owner){
+        super(owner);
+        cost = 1; //wird in den unter klassen überschrieben
+        cooldown = 1500; //in Millisekunden
+        duration = 100; //in Millisekunden
+    }
+    @Override
+    public String getDescription(){ return "+ 15% damage";}
+    @Override
+    public void unlock(){
+        unlocked = true;
+        owner.setDamageModifier(owner.getDamageModifier()+15);
+    }
+
+    @Override
+    public boolean use(){
+        if (!super.use()){ return false; } //offcooldown check
+        return true;
+    }
+
+
+}
